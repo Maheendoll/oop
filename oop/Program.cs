c0a4b5ea842c61b71fb521701826e41d3306f471
Maheenqamar <mq254411@gmail.com>
2024-11-02 02:07:17 +0500

    Update Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -10,7 +10,64 @@ namespace oop
     {
         static void Main(string[] args)
         {
-            njnn
+            class Dog
+    {
+        public string Name;
+        public int Age;
+
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} says: Woof!");
+        }
+    }
+
+    class Human
+    {
+        public string Name;
+        public int Age;
+        public string gender;
+        public string country;
+
+        public void Run()
+        {
+            Console.WriteLine($"{Name} is Running");
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} is Eating");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Dog object
+            // Dog myDog = new Dog();
+            //  myDog.Name = "Buddy";
+            //  myDog.Age = 3;
+
+            // Call the Bark method
+            // myDog.Bark();
+
+            // Display dog's information
+            // Console.WriteLine($"My dog's name is {myDog.Name} and he is {myDog.Age} years old.");
+
+            Human myHuman = new Human();
+
+            myHuman.Name = "Maaz";
+            myHuman.Age = 16;
+            myHuman.gender = "Male";
+            myHuman.country = "Pakistan";
+
+
+            myHuman.Run();
+
+            myHuman.Eat();
+
+            Console.WriteLine($"My name is {myHuman.Name} and i am {myHuman.Age} years old. i am {myHuman.gender} and i am living in {myHuman.country}.");
+            Console.ReadKey();
+        }
+    }
         }
     }
 }
